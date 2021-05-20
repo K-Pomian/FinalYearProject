b8ea8754add3af7aa599077fc8076c9c2844c69d
K-Pomian <pomian.konrad@gmail.com>
2021-05-20 12:14:14 +0200

    StatusMismatchException added.

---
+++ FinalYearProject/src/finalyearproject/exceptions/StatusMismatchException.java
@@ -0,0 +1,14 @@
+package finalyearproject.exceptions;
+
+public class StatusMismatchException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7973533075349689596L;
+
+	public StatusMismatchException(String message) {
+		super(message);
+	}
+
+}
